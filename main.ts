8d02af7ce9f01dc4c62f812a0061f5fc46c34569
RLambini <105668093+RLambini@users.noreply.github.com>
2022-05-22 14:00:30 +0200

    Python conversion

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 let Qibla = 0
-basic.forever(function () {
+basic.forever(function on_forever() {
+    
     Qibla = input.compassHeading()
     if (Qibla <= 145 && Qibla >= 125) {
         basic.showIcon(IconNames.Yes)
     } else {
         basic.showIcon(IconNames.No)
     }
+    
 })
